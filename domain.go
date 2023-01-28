b52ef9e532275ae396556ff5b459d84ddd520258
Patrik Kernstock <patrik@kernstock.net>
2023-01-28 17:10:31 +0000

    Fix wrong type crash of count in Domain.List

--- domain.go
+++ domain.go
@@ -3,6 +3,7 @@ package goinwx
 import (
 	"errors"
 	"time"
+	"strconv"
 
 	"github.com/fatih/structs"
 	"github.com/mitchellh/mapstructure"
@@ -140,6 +141,19 @@ func (s *DomainService) List(request *DomainListRequest) (*DomainList, error) {
 	resp, err := s.client.Do(req)
 	if err != nil {
 		return nil, err
+	}
+
+	// This is a (temporary) workaround to convert the API response from string to int.
+	// The code only applies when string is being return, otherwise it's being skipped.
+	// As per docs at https://www.inwx.com/en/help/apidoc/f/ch02s08.html#domain.list we
+	// should get int here, but apparently it's not the case.
+	// Ticket 10026265 with INWX was raised.
+	if countStr, ok := (*resp)["count"].(string); ok {
+		// If we land here, we got string back but we expect int.
+		// Converting value to int and writing it to the response.
+		if num, ok := strconv.Atoi(countStr); ok == nil {
+			(*resp)["count"] = num
+		}
 	}
 
 	var result DomainList
